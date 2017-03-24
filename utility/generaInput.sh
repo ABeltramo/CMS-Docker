1b8781817e2ae78edad3387405e41dbc912a68b8
ABeltramo <beltramo.ale@gmail.com>
2017-03-24 10:49:08 +0100

    Added c++11

--- utility/generaInput.sh
+++ utility/generaInput.sh
@@ -1,24 +1,26 @@
 #! /bin/bash
 
 genfile="$1/gen/GEN"
-generatore="$1/gen/generatore.py"
-risolutore="$1/sol/soluzione.cpp"
+generatore="python $1/gen/generatore.py"
+risolutoreFile="$1/sol/soluzione.cpp"
+risolutore="$1/sol/soluzione"
 testo="$1/testo/testo.tex"
 
 mkdir "$1/input"
 mkdir "$1/output"
 i=0
-g++ $risolutore -o $1/sol/soluzione
+g++ -std=c++11 $risolutoreFile -o $1/sol/soluzione
 while read line; do
   case "$line" in 		  # Skip
 	\#*) continue ;; 	    # Comment
       	"") continue ;; # Empty line
   esac
   echo $i
-  python $generatore $line > $1/input/"input${i}.txt"
-  cat $1/input/"input${i}.txt" | $1/sol/soluzione > $1/output/"output${i}.txt"
+  $generatore $line > $1/input/"input${i}.txt"
+  cat $1/input/"input${i}.txt" | $risolutore > $1/output/"output${i}.txt"
   i=$(($i+1))
 done < $genfile
 
 # Compile pdf in folder testo
-pdflatex -output-directory $1/testo $testo
+pdflatex -output-directory $1/testo $testo > $1/testo/consoleLog
+echo "DONE!"
