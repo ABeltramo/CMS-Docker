612634365dab18404c69255bfcce4950e7c46c5c
ABeltramo <beltramo.ale@gmail.com>
2018-02-01 11:33:06 +0100

    BugFix

--- db/startup.sh
+++ db/startup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
+echo "******* RUNNING NON DEFAULT INIT ********"
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE cmsdb;
+    GRANT ALL PRIVILEGES ON DATABASE cmsdb TO cmsuser;
     ALTER SCHEMA public OWNER TO cmsuser
     GRANT SELECT ON pg_largeobject TO cmsuser
 EOSQL
